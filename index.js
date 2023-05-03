c1b3d15c9a57b7d13596b1fc46a97879046aed6b
athaimahbub <athaimahbub2016@gmail.com>
2023-05-03 19:47:22 +0600

    checking

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// const chefDetails = require('./data/chefBio.json');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// app.get('/chefBio', (req, res) => {
+//     res.send(chefDetails);
+//   })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
